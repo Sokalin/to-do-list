e1435ca1b072e5f53c148f2103060b90393fe383
XD <dhhugtu2@gmail.com>
2024-07-10 00:06:31 +0300

    Data_moderls undated

--- data_models.py
+++ data_models.py
@@ -1,14 +1,40 @@
+from datetime import datetime
+
+
 class Task:
-    pass
+    _task_id = None
+    _task_time = None
+    _task_priority = None
+    _task_value = None
+    _task_amount = 0
+
+    def __new__(cls, *args, **kwargs):
+        Task._task_amount += 1
+        return super().__new__(cls)
+
+    def __init__(self, value=''):
+        self._task_value = value
+        self._task_id = self._task_amount
+        self._task_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    def show_data(self):
+        print(f'{self._task_priority}. {self._task_value} [{self._task_id}/{self._task_amount}] - {self._task_time}')
 
 
 class TaskPriority1(Task):
-    pass
+    _task_priority = 1
+
+
+    # def __new__(cls, *args, **kwargs):
+    #     cls._task_priority1_amount += 1
+    #     return super().__new__(cls)
+
+
 
 
 class TaskPriority2(Task):
-    pass
+    _task_priority = 2
 
 
 class TaskPriority3(Task):
-    pass
+    _task_priority = 3
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import data_models
+
 
 if __name__ == '__main__':
-    pass
+    a1 = data_models.TaskPriority1('sleep')
+    a1.show_data()
+    a2 = data_models.TaskPriority1('drink coffe')
+    a2.show_data()
+    a3 = data_models.TaskPriority3()
+    a3.show_data()
+
+    for a in [a1, a2, a3]:
+
+        a.show_data()
+
